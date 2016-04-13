c0951bfbe23f36429dbb5f5f3e66afb7e8f13888
Jorge Mendizabal <jorgemendiza@me.com>
2016-04-13 15:38:33 -0500

    Agregada una funcion para testear la conexion a la base de datos mlab

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,14 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('newFunction', function(req, res) {
+	var query_mem = new Parse.Query("TestObject");
+	query_mem.find({
+		success: function (objects) {
+			res.success(objects);
+		}, error: function (error) {
+			res.error(error);
+		}
+	});
+});
