d7a89b7082e2fadf8271b79a84af3ecca9bc9471
Jorge Mendizabal <jorgemendiza@me.com>
2016-04-20 15:17:42 -0500

    otro test

--- cloud/main.js
+++ cloud/main.js
@@ -104,8 +104,24 @@ Parse.Cloud.define('registrarPaciente', function(req, res) {
 
 
 Parse.Cloud.define('registrarEspecialista', function(req, res) {
-	
-	  res.success('Hi');
+	var user = new Parse.User();
+	user.set("username", "my name");
+	user.set("password", "my pass");
+	user.set("email", "email@example.com");
+
+	// other fields can be set just like with Parse.Object
+	user.set("phone", "415-392-0202");
+
+	user.signUp(null, {
+	  success: function(user) {
+	    // Hooray! Let them use the app now.
+	res.success(user);
+	  },
+	  error: function(user, error) {
+	    // Show the error message somewhere and let the user try again.
+			res.error(error);	   
+	  }
+	});
 });
 
 function getUserById(pacienteId , callback) {
